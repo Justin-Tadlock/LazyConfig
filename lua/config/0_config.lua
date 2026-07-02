e44c29f8b10ae86cd13c1462a675f4e9e0b285ed
Justin Tadlock <toukenlogins@pm.me>
2026-07-01 22:51:55 -0700

    Adding basic config options

---
+++ lua/config/0_config.lua
@@ -0,0 +1,23 @@
+if vim.loader then
+  vim.loader.enable()
+end
+
+-- I don't like having a split clipboard buffer from system and different nvim buffers
+vim.schedule(function()
+  vim.o.clipboard = "unnamedplus"
+end)
+
+-- Sometimes I'm lazy and just use the mouse..
+vim.o.mouse = "a"
+
+vim.o.breakindent = true
+vim.o.undofile = true
+
+-- Search case-insensitive by default unless a Casing was used
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.signcolumn = "yes"
+vim.o.inccommand = "split"
+vim.o.cursorline = true
+vim.o.confirm = true
